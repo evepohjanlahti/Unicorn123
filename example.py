010830d4f9c5f90a58307273ed054e35b02b237a
Eve Pohjanlahti <tuevpohj@student.jyu.fi>
2018-05-31 16:14:24 +0300

    Testataan  Travisia ja coveralls

--- example.py
+++ example.py
@@ -24,10 +24,10 @@ def get_word_lengths(s):
 
 
 # uncomment this function in step 6
-#def test_get_word_lengths():
-#    text = "Three tomatoes are walking down the street"
-#    assert get_word_lengths(text) == [5, 8, 3, 7, 4, 3, 6]
-
+def test_get_word_lengths():
+    text = "Three tomatoes are walking down the street"
+    assert get_word_lengths(text) == [5, 8, 3, 7, 4, 3, 6]
+m = words
 
 def obscure_function():
     """
